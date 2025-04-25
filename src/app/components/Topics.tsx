f9770f08ad18805b7b2b98ec32a7e7c45bf0bb03
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-04-25 19:45:55 +0800

    crud-project

---
+++ src/app/components/Topics.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Topics = () => {
+  return (
+    <>
+    <div>
+      <div>
+        <h1>Topic Title</h1>
+        <div>Topic Description</div>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Topics
---
+++ src/app/route/workspace/page.tsx
@@ -0,0 +1,12 @@
+import Topics from '@/app/components/Topics'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="">
+      <Topics/>
+    </div>
+  )
+}
+
+export default page
