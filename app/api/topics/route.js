c09dc425807eab503d4ea4b629fbfc4f9382d05b
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-03 21:12:14 +0800

    created get and delete function

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -2,6 +2,8 @@ import connectMongoDB from "../../../libs/mongodb";
 import Topic from "../../../models/topic";
 import { NextResponse } from "next/server";
 //here we do crud operations
+
+//POST function
 export async function POST(request){
     //get data from request
     const {title, description} = await request.json();
@@ -13,4 +15,19 @@ export async function POST(request){
     return NextResponse.json({message: "Topic Created"}, {status: 201})
 }
 
+//GET function 
+export async function GET(){
+    await connectMongoDB();
+    await Topic.find(); //find all the topics and return it
+    const topics = await Topic.find(); //store find topic inside 
+    return NextResponse.json({topics})
+}
+
+//DELETE function
+export async function DELETE(request){ 
+    const id = request.nextUrl.searchParams.get('id'); //sending the id of the topic as a search parameter for this context
+    await connectMongoDB();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json({message: "Topic Deleted"}, {status: 200})
+}
 //use postman to check if api works
