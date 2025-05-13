5d043efb4971a7ef5948c13d3c45615666095f0c
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-13 16:12:52 +0800

    update function

--- app/components/EditTopicForm.tsx
+++ app/components/EditTopicForm.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
 
-const EditTopicForm = () => {
+
+const EditTopicForm = ({id, title, description}) => { //send topic id, title, and description here as props
+
+  const [newTitle, setNewTitle] = useState(title); //newtitle = value, setNewTitle = onchange, binded to input
+  const [newDescription, setNewDescription] = useState(description);
+  
+  const router = useRouter();
+
+  //function for handling the submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try{
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type" : "application/json",
+        },
+        body: JSON.stringify({newTitle,newDescription}),
+      });
+
+      if(!res.ok){
+        throw new Error("Failed to update topic");
+      }
+
+      router.refresh(); //updates the data as soon as it gets updated
+      router.push('/')
+    }catch(error){
+      console.log(error)
+    }
+   
+  }
   return (
-    <form className='flex flex-col gap-3'>
-        <input className="border border-slate-500 px-8 py-2" type='text' placeholder='Topic Title'></input>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+        <input value={newTitle} onChange={e => setNewTitle(e.target.value)} className="border border-slate-500 px-8 py-2" type='text' placeholder='Topic Title'></input>
 
-        <input className="border border-slate-500 px-8 py-2" type='text' placeholder='Topic Description'></input>
+        <input value={newDescription} onChange={e => setNewDescription(e.target.value)} className="border border-slate-500 px-8 py-2" type='text' placeholder='Topic Description'></input>
 
         <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit cursor-pointer'>
             Update Topic
--- app/editTopic/[id]/page.tsx
+++ app/editTopic/[id]/page.tsx
@@ -1,9 +1,32 @@
+import { error } from 'console';
 import EditTopicForm from '../../components/EditTopicForm'
 import React from 'react'
 
-const page = () => {
+const getTopicById = async(id) => { //function to get topic
+  try{
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+     cache: "no-store",
+    });
+
+    if(!res.ok){
+      throw new Error('Failed to fetch topic');
+    }
+    
+    return res.json();
+  }catch(error){
+    console.log(error)
+  }
+
+}
+
+const page = async ({params}) => {
+
+  const {id} = params;
+  const {topic} = await getTopicById(id); //call function
+  const {title, description} = topic;
+
   return (
-    <EditTopicForm/>
+    <EditTopicForm id={id} title={title} description={description}/>
   )
 }
 
